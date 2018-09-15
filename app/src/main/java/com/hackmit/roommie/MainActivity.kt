a143ce781c2908129a4e19412cd9a6b1c99d8747
Eva Huang <ehuang42@gatech.edu>
2018-09-15 16:44:35 -0400

    something works

--- app/src/main/java/com/hackmit/roommie/MainActivity.kt
+++ app/src/main/java/com/hackmit/roommie/MainActivity.kt
@@ -10,6 +10,7 @@ import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -24,7 +25,6 @@ class MainActivity : AppCompatActivity() {
 
 
         val loginBtn = findViewById<View>(R.id.login_btn) as Button
-        val signupBtn = findViewById<View>(R.id.signup_btn)as Button
         loginBtn.setOnClickListener(View.OnClickListener {
             view -> login()
         })
@@ -46,9 +46,9 @@ class MainActivity : AppCompatActivity() {
         var password = passwordTxt.text.toString().trim()
 
 
-        if (!email.isEmpty() && !password.isEmpty() && user!=null) {
+        if (!email.isEmpty() && !password.isEmpty()) {
             this.mAuth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener (this, OnCompleteListener<AuthResult> { task ->
+                        .addOnCompleteListener (this, OnCompleteListener<AuthResult> { task ->
                 if (task.isSuccessful) {
                     startActivity(Intent(this, Timeline::class.java))
                     Toast.makeText(this, "Successfully Logged in :)", Toast.LENGTH_LONG).show()
