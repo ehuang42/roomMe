1a1bb4a3cc2c3a9776eb8552dcc1b2d70fd25da9
Alexa Flesch <alexaflesch@gmail.com>
2018-09-16 08:20:15 +0000

    Register.kt edited online with Bitbucket

--- app/src/main/java/com/hackmit/roommie/Register.kt
+++ app/src/main/java/com/hackmit/roommie/Register.kt
@@ -12,7 +12,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.hackmit.roommie.Controllers.QuestListActivity
-
+import com.google.firebase.database.ValueEventListener
 class register : AppCompatActivity() {
 
     val mAuth = FirebaseAuth.getInstance()
